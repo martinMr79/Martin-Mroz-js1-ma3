093cb5f3d7d5eba806e8d2176bbaf803baf866ce
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-02-10 13:12:49 +0100

    10.02

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+//function getRemainder(a,b) {
+//    return a % b;
+//    }
+
+// Converted to arrow function:
+
+const getRemainder = (a, b) => a % b;
---
+++ js/question-2.js
@@ -0,0 +1,4 @@
+
+
+//GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
+//GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
