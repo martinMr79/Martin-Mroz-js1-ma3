fc9ca991a94756653aefe67873c35a1fb89091b2
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-02-11 15:01:44 +0100

    11.02

--- js/question-2.js
+++ js/question-2.js
@@ -2,31 +2,38 @@ const keyURL =
   "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=c0df2dfdfec944839c0adc50e065f7c6";
 
 const container = document.querySelector(".container");
+const loader = document.querySelector(".loader")
 
 async function getRawg() {
   const response = await fetch(keyURL);
   const list = await response.json();
   const games = list.results;
   
+  loader.innerHTML = "";
 
-  
+  for (let i = 0; i < games.length; i++) {
+    console.log(
+      games[i].name + " " + games[i].rating + " " + games[i].tags.length
+    );
 
-    for (let i = 0; i < games.length; i++) {
-      console.log(games[i].rating + " " + games[i].name);
-      container.innerHTML += `${games[i].rating + " " + games[i].name}`
+    if (i === 7) {
+      break;
     }
-      
-  //  if (i === 8) {
-  //    break;
-  // }
 
-  //
-  // }
+    container.innerHTML += `<div class="container"> ${
+      games[i].name + " " + games[i].rating + " " + games[i].tags.length
+    }</div>`;
+  }
 }
 
-getRawg();
+const error = document.querySelector(".error");
+
+try {
+  getRawg();
+  console.log("Yeey");
+} catch (error) {
+  console.log("Oh no! An error occurred", error);
+  result.innerHTML = "Oh no! An error occurred";
+}
 
 // API Key :  c0df2dfdfec944839c0adc50e065f7c6
-
-//GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
-//GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
